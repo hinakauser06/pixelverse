848229e228bb5a1d3b321dee3b0eafe1da44937f
hinakauser06 <163968444+hinakauser06@users.noreply.github.com>
2024-03-20 00:41:32 +0530

    Add files via upload

---
+++ JS/about.js
@@ -0,0 +1,21 @@
+const list = document.getElementById("lightSwitch")
+let body = document.getElementById("body");
+
+list.addEventListener('change', (data) => {
+    console.log("called here 111", list.checked)
+
+    if(list.checked){
+        document.body.classList.remove('bg-light');
+        document.body.classList.remove('text-dark');
+        document.body.classList.add('bg-dark');
+        document.body.classList.add('text-light');
+
+    }else{
+
+        document.body.classList.remove('bg-dark')
+        document.body.classList.remove('text-dark')
+        document.body.classList.add('bg-light');
+        document.body.classList.add('text-dark');   
+
+    }
+})
